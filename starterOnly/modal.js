5378596854964a29b523f53c9dd13eaa88596e29
Gautier De Coninck <gautier.deconinck@gmail.com>
2021-03-30 09:46:07 +0200

    added : for & id in html, name and email check in formular

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,3 +30,41 @@ function closeModal() {
 }
 
 
+//function check if it's empty
+function nameCheked(value) {
+  console.log('hello',value);
+    if(value != "" && value.length > 2){
+      return true;
+    } else {
+      alert("le champ ne peut pas être vide");
+      return false;
+    }
+}
+
+//function check if it's an email
+function isEmail(value){
+  const regEmail = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i');
+
+  if (regEmail.test(value)){    
+    return true;
+  } else {
+    alert('adresse mail invalide');
+    return false;
+  }
+}
+
+//validation of the formular
+
+function validate(){
+  console.log('hey', document.forms['reserve']['first'].value);
+  nameCheked(document.forms['reserve']['first'].value);
+  nameCheked(document.forms['reserve']['last'].value);
+  isEmail(document.forms['reserve']['email'].value);
+
+
+
+
+
+
+
+}
